415bd2057e5704a89d8ea92340b8c6b9e2f0497e
Kozor1 <r.m.lavery@hotmail.co.uk>
2023-04-25 13:00:41 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -6,12 +6,19 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  const name = "Anna Stehr";
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+  const index = niceList.indexOf(name);
+  const proof = merkleTree.getProof(index);
 
+  if ( index != 1) {
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      proof,
+      name,
+    });
   console.log({ gift });
+  }
 }
 
 main();
